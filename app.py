49efd9bd0da6a8757fdae92a1803c9ddb6438430
Tianhao Zhou <tianhaoz@umich.edu>
2021-11-13 22:50:35 -0800

    feat: add spending analysis

--- app.py
+++ app.py
@@ -1,17 +1,81 @@
 import streamlit as st
 import pandas as pd
-from inputs import (fetch_crypto_wallet_history, fetch_fiat_wallet_history,
-                    fetch_visa_card_history, fetch_native_currency)
+from inputs import (fetch_crypto_wallet_history, fetch_feature_flags,
+                    fetch_fiat_wallet_history, fetch_visa_card_history)
+
+st.set_page_config(initial_sidebar_state='expanded')
 
 st.title('Crypto.com History Analysis')
 
-st.sidebar.title('Settings')
 crypto_wallet_history = fetch_crypto_wallet_history()
 fiat_wallet_history = fetch_fiat_wallet_history()
 visa_card_history = fetch_visa_card_history()
-native_currency = fetch_native_currency()
+features = fetch_feature_flags()
 
-combined_history = pd.concat([crypto_wallet_history, fiat_wallet_history, visa_card_history])
+combined_history = pd.concat(
+    [crypto_wallet_history, fiat_wallet_history, visa_card_history])
 
+st.header('Overview')
 if not combined_history.empty:
-    st.write(combined_history)
+    investment_histroy = combined_history.copy()
+    investment_histroy = investment_histroy[
+        (investment_histroy['Currency'] == 'USD')
+        & (investment_histroy['Transaction Kind'] == 'van_purchase')]
+    if 'summary' in features:
+        st.subheader('Summary')
+        col1, col2, col3 = st.columns(3)
+        total_investment = -investment_histroy['Amount'].sum()
+        col1.metric(label='Total Investment',
+                    value='$ {0}'.format(int(total_investment)))
+        staking_reward = combined_history[combined_history['Transaction Kind']
+                                          ==
+                                          'mco_stake_reward']['Amount'].sum()
+        col2.metric(label='CRO Staking Reward',
+                    value='{0} CRO'.format(int(staking_reward)))
+        cashback = combined_history[combined_history['Transaction Kind'] ==
+                                    'referral_card_cashback']['Amount'].sum()
+        col3.metric(label='Cashback Reward',
+                    value='{0} CRO'.format(int(cashback)))
+    if 'raw_table' in features:
+        st.subheader('Combined Table')
+        st.write(combined_history)
+        st.subheader('Initial Investment Table')
+        st.write(investment_histroy)
+
+st.header('Visa Card')
+if not visa_card_history.empty:
+    if 'raw_table' in features:
+        st.subheader('Visa Card Transaction Table')
+        st.write(visa_card_history)
+    if 'spending_by_category' in features:
+        spending_by_categories_table = visa_card_history.copy()
+        spending_by_categories_table = spending_by_categories_table[spending_by_categories_table['Amount'] < 0]
+        spending_by_categories_table['Amount'] = spending_by_categories_table['Amount'] * -1
+        spending_by_categories_table = spending_by_categories_table[[
+            'Transaction Description', 'Amount'
+        ]].groupby(['Transaction Description']).sum()
+        st.subheader('Spending by Category')
+        st.bar_chart(spending_by_categories_table)
+    if 'monthly_spending' in features:
+        spending = visa_card_history.copy()
+        spending = spending[spending['Native Amount'] < 0][['Native Amount'
+                                                            ]].copy()
+        spending['Native Amount'] = spending['Native Amount'].apply(
+            lambda x: -x)
+        monthly_spending = spending.groupby(pd.Grouper(freq='M')).sum()
+        st.subheader('Monthly spending')
+        st.bar_chart(monthly_spending)
+else:
+    st.warning('Visa card history has not been uploaded')
+
+st.header('Crypto Wallet')
+if not crypto_wallet_history.empty:
+    pass
+else:
+    st.warning('Crypto wallet history has not been uploaded')
+
+st.header('Fiat Wallet')
+if not fiat_wallet_history.empty:
+    pass
+else:
+    st.warning('Fiat wallet history has not been uploaded')
--- inputs.py
+++ inputs.py
@@ -1,12 +1,13 @@
-from typing import List
 import pandas as pd
 import streamlit as st
 
 
 def fetch_data(hint: str) -> pd.DataFrame:
     uploaded_file = st.sidebar.file_uploader(label=hint)
-    return pd.read_csv(
-        uploaded_file) if uploaded_file is not None else pd.DataFrame()
+    if uploaded_file is not None:
+        return construct_timestamp_index(pd.read_csv(uploaded_file))
+    else:
+        return pd.DataFrame()
 
 
 def fetch_crypto_wallet_history() -> pd.DataFrame:
@@ -21,7 +22,16 @@ def fetch_visa_card_history() -> pd.DataFrame:
     return fetch_data('Visa Card Histrory (CSV)')
 
 
-def fetch_native_currency() -> List[str]:
-    selected_native_currency = st.sidebar.multiselect(
-        'What is your native currency?', ['USD', 'CAD'], ['USD'])
-    return selected_native_currency
+def construct_timestamp_index(data: pd.DataFrame) -> pd.DataFrame:
+    data.index = pd.to_datetime(data['Timestamp (UTC)'],
+                                format='%Y-%m-%d %H:%M:%S')
+    return data.drop(columns=['Timestamp (UTC)'])
+
+
+def fetch_feature_flags() -> set[str]:
+    default_features = ['summary', 'monthly_spending', 'spending_by_category']
+    optional_features = ['raw_table']
+    return set(
+        st.sidebar.multiselect('Features',
+                               default_features + optional_features,
+                               default_features))
