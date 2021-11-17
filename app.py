cda889cf40857032f038b0e1ca09a75b6fa06d40
Tianhao Zhou <tianhaoz@umich.edu>
2021-11-16 22:16:29 -0800

    feat: add direct purchase

--- app.py
+++ app.py
@@ -21,26 +21,51 @@ if not combined_history.empty:
     investment_histroy = investment_histroy[
         (investment_histroy['Currency'] == 'USD')
         & (investment_histroy['Transaction Kind'] == 'van_purchase')]
+    staking_reward = combined_history[combined_history['Transaction Kind'] ==
+                                      'mco_stake_reward']['Amount'].sum()
+    cashback = combined_history[combined_history['Transaction Kind'] ==
+                                'referral_card_cashback']['Amount'].sum()
+    mission_reward = combined_history[
+        combined_history['Transaction Description'] ==
+        'Mission Rewards Deposit']['Amount'].sum()
+    direct_purchase_table = combined_history[
+        combined_history['Transaction Description'].str.contains('^Buy ')]
+    direct_purchase_investment = direct_purchase_table['Native Amount'].sum()
+    estimated_direct_purchase_fees = direct_purchase_investment * 1.25 / 100
+    fiat_purchase = -investment_histroy['Amount'].sum()
+    total_investment = direct_purchase_investment + fiat_purchase
+    total_cro_rewards = cashback + mission_reward + staking_reward
     if 'summary' in features:
         st.subheader('Summary')
-        col1, col2, col3 = st.columns(3)
-        total_investment = -investment_histroy['Amount'].sum()
-        col1.metric(label='Total Investment',
-                    value='$ {0}'.format(int(total_investment)))
-        staking_reward = combined_history[combined_history['Transaction Kind']
-                                          ==
-                                          'mco_stake_reward']['Amount'].sum()
-        col2.metric(label='CRO Staking Reward',
-                    value='{0} CRO'.format(int(staking_reward)))
-        cashback = combined_history[combined_history['Transaction Kind'] ==
-                                    'referral_card_cashback']['Amount'].sum()
-        col3.metric(label='Cashback Reward',
-                    value='{0} CRO'.format(int(cashback)))
+        r1 = st.columns(3)
+        r1[0].metric(label='Total Investment',
+                     value='$ {0}'.format(int(total_investment)))
+        r1[1].metric(label='Direct Purchase Amount (Fee)',
+                     delta='-${0} Fee (estimated at 1.25%)'.format(
+                         int(estimated_direct_purchase_fees)),
+                     value='$ {0}'.format(int(direct_purchase_investment)))
+        r1[2].metric(label='Fiat Purchase Amount',
+                     value='$ {0}'.format(int(fiat_purchase)))
+        st.info('''
+                Fiat purchase: crypto purchase made from the fiat wallet which in general do not incur fees.\n
+                Direct purchase: crypto purchase made from credit or debit card which has a fee of roughly 1.25%.
+                ''')
+        st.metric(label='Total CRO Reward (excluding earn)',
+                  value='{0} CRO'.format(int(total_cro_rewards)))
+        reward_row = st.columns(3)
+        reward_row[0].metric(label='CRO Staking Reward',
+                             value='{0} CRO'.format(int(staking_reward)))
+        reward_row[1].metric(label='Cashback Reward',
+                             value='{0} CRO'.format(int(cashback)))
+        reward_row[2].metric(label='Total Mission Reward',
+                             value='{0} CRO'.format(int(mission_reward)))
     if 'raw_table' in features:
         st.subheader('Combined Table')
         st.write(combined_history)
         st.subheader('Initial Investment Table')
         st.write(investment_histroy)
+        st.subheader('Direct Purchase Table')
+        st.write(direct_purchase_table)
 
 st.header('Visa Card')
 if not visa_card_history.empty:
@@ -49,8 +74,10 @@ if not visa_card_history.empty:
         st.write(visa_card_history)
     if 'spending_by_category' in features:
         spending_by_categories_table = visa_card_history.copy()
-        spending_by_categories_table = spending_by_categories_table[spending_by_categories_table['Amount'] < 0]
-        spending_by_categories_table['Amount'] = spending_by_categories_table['Amount'] * -1
+        spending_by_categories_table = spending_by_categories_table[
+            spending_by_categories_table['Amount'] < 0]
+        spending_by_categories_table[
+            'Amount'] = spending_by_categories_table['Amount'] * -1
         spending_by_categories_table = spending_by_categories_table[[
             'Transaction Description', 'Amount'
         ]].groupby(['Transaction Description']).sum()
